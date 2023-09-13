82b6e966fd153d19374424b96e0579bde80e320c
Tony W <bobacrypto2@gmail.com>
2023-09-13 00:19:21 -0700

    Add connection to our MongoDB Database

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,20 @@ const jwt = require("jsonwebtoken");
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
 const User = require("./models/user");
 const postRouter = require("./routes/posts");
 const commentRouter = require("./routes/comments");
 const userRouter = require("./routes/users");
+
+mongoose.set("strictQuery", false);
+
+const mongoDB = process.env.MONGODB_SECRET;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
 
 const app = express();
 
@@ -57,21 +67,21 @@ app.use("/post", postRouter);
 app.use("/comment", commentRouter);
 app.use("/user", userRouter);
 
-function verifyToken(req, res, next) {
-  const bearerHeader = req.headers["authorization"];
+// function verifyToken(req, res, next) {
+//   const bearerHeader = req.headers["authorization"];
 
-  if (typeof bearerHeader !== "undefined") {
-    // Split Bearer <access_token> at the space
-    const bearer = bearerHeader.split(" ");
+//   if (typeof bearerHeader !== "undefined") {
+//     // Split Bearer <access_token> at the space
+//     const bearer = bearerHeader.split(" ");
 
-    const bearerToken = bearer[1];
+//     const bearerToken = bearer[1];
 
-    req.token = bearerToken;
-    next();
-  } else {
-    res.sendStatus(403);
-  }
-}
+//     req.token = bearerToken;
+//     next();
+//   } else {
+//     res.sendStatus(403);
+//   }
+// }
 
 app.listen(3000, () => console.log("Server started on 3000"));
 
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,16 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 // Create User
-router.get("/sign-up", ADD.CONTROLLER);
+router.get("/sign-up");
 router.post("/sign-up", ADD.CONTROLLER);
 
 // Delete User
 router.get("/delete/:userId", ADD.CONTROLLER);
 router.post("/delete/:userId", ADD.CONTROLLER);
-
-// Update User
-router.get("/update/:userId", ADD.CONTROLLER);
-router.post("/update/:userId", ADD.CONTROLLER);
 
 // Get a list of Users
 router.get("/list", ADD.CONTROLLER);
