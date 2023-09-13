1a6741c02c529c299da11e8bc0b9b4984bdea612
Tony W <bobacrypto2@gmail.com>
2023-09-13 00:06:38 -0700

    Add passport middleware for username password authentication | Add logic for user sign up controller including bcrypt for hashing passwords

---
+++ backend/controller/userController.js
@@ -0,0 +1,86 @@
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const { body, validationResult } = require("express-validator");
+const asyncHandler = require("express-async-handler");
+
+exports.user_list_get = asyncHandler(async (req, res, next) => {
+  const userList = await User.find({}, "username").sort({ username: 1 }).exec();
+
+  res.json({
+    user_list: userList,
+  });
+});
+
+exports.user_create_post = [
+  // Sanitize and Validate sign up fields
+  body("username", "Username 3 characters min.")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  body("password", "Password 4 characters min.")
+    .trim()
+    .isLength({ min: 4 })
+    .escape(),
+  body("confrimPassword", "Password 4 characters min.")
+    .trim()
+    .isLength({ min: 4 })
+    .custom(async (value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Passwords must match!");
+      }
+      return true;
+    })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.json({ error: errors.array() });
+    }
+
+    // Check if User already exists
+    const existingUser = await User.find({
+      username: req.body.username,
+    }).exec();
+    if (existingUser) {
+      return res.status(400).json({
+        error: "Username already exists.",
+      });
+    }
+
+    if (req.body.password !== req.body.confirmPassword) {
+      return res.status(401).json({
+        error: "Passwords must match!",
+      });
+    }
+
+    bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+      if (err) return next(err);
+
+      const user = new User({
+        username: req.body.username,
+        password: hashedPassword,
+      });
+      await user.save();
+
+      jwt.sign(
+        { user: user },
+        process.env.REFRESH_KEY,
+        { expiresIn: "5m" },
+        (err, token) => {
+          if (err) return next(err);
+
+          return res.status(200).json({
+            token,
+            user: {
+              username: user.username,
+            },
+            message: "Success! Account created.",
+          });
+        }
+      );
+    });
+  }),
+];
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,55 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("./models/user");
 const postRouter = require("./routes/posts");
 const commentRouter = require("./routes/comments");
 const userRouter = require("./routes/users");
 
 const app = express();
 
+passport.use(
+  new LocalStrategy(async (username, passport, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+      const match = await bcrypt.compare(password, user.password);
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      if (!match) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async function (id, done) {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+app.use(
+  session({
+    secret: process.env.REFRESH_KEY,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
