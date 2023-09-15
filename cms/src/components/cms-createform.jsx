25467643ca17760f45b110e21dc9853dd2d89318
Tony W <bobacrypto2@gmail.com>
2023-09-14 18:20:59 -0700

    Style form for creating posts

--- cms/src/components/cms-createform.jsx
+++ cms/src/components/cms-createform.jsx
@@ -2,10 +2,28 @@ import "../styles/cms-createform.css";
 
 const CmsCreate = () => {
   return (
-    <form>
-      <label>
-        <input></input>
+    <form id="post-form">
+      <label htmlFor="post-title">
+        <input
+          id="post-title"
+          name="post-title"
+          type="text"
+          placeholder="Title of post"
+        ></input>
       </label>
+      <label htmlFor="post-body">
+        <textarea
+          id="post-body"
+          name="post-body"
+          placeholder="(Max chars. 427)"
+          minLength={25}
+          maxLength={427}
+        ></textarea>
+      </label>
+      <div id="post-form-btns">
+        <button id="postBtn">Post</button>
+        <button id="clearBtn">Clear</button>
+      </div>
     </form>
   );
 };
