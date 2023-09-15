93935340f550e974588eeb4463d4ae23d77abbce
Tony W <bobacrypto2@gmail.com>
2023-09-14 18:28:41 -0700

    Add function to CmsNav component to change styles between button upon clicking

--- cms/src/components/cms-nav.jsx
+++ cms/src/components/cms-nav.jsx
@@ -1,18 +1,45 @@
 import "../styles/cms-nav.css";
 
 const CmsNav = () => {
+  function changeBtnState(btn) {
+    if (btn.target.className.includes("cms-active")) {
+      return;
+    }
+    const activeBtn = document.querySelector(".cms-active");
+
+    activeBtn.classList.remove("cms-active");
+    activeBtn.classList.add("cms-inactive");
+    btn.target.classList.remove("cms-inactive");
+    btn.target.classList.add("cms-active");
+  }
   return (
     <nav id="cms-nav">
-      <button id="cms-create" className="cms-button cms-active">
+      <button
+        id="cms-create"
+        className="cms-button cms-active"
+        onClick={(btn) => changeBtnState(btn)}
+      >
         Create
       </button>
-      <button id="cms-delete" className="cms-button cms-inactive">
+      <button
+        id="cms-delete"
+        className="cms-button cms-inactive"
+        onClick={(btn) => changeBtnState(btn)}
+      >
         Delete
       </button>
-      <button id="cms-update" className="cms-button cms-inactive">
+      <button
+        id="cms-update"
+        className="cms-button cms-inactive"
+        onClick={(btn) => changeBtnState(btn)}
+      >
         Update
       </button>
-      <button id="cms-view-all" className="cms-button cms-inactive">
+      <button
+        id="cms-view-all"
+        className="cms-button cms-inactive"
+        onClick={(btn) => changeBtnState(btn)}
+      >
         View Posts
       </button>
     </nav>
