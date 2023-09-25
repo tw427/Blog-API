e935b8c57027f3e60018cd41a18c0d02830d90b6
Tony W <bobacrypto2@gmail.com>
2023-09-24 20:28:57 -0700

    Progress styling of Home component for our Home Page's posts view

--- frontend/components/home.jsx
+++ frontend/components/home.jsx
@@ -3,12 +3,24 @@ export const Home = () => {
     <>
       <main id="view-home">
         <section id="left-home">
-          <div className="left-test1">dummy text</div>
-          <div className="left-test2">dummy text</div>
+          <div className="left test1">
+            <img></img>
+            <p>dummy text</p>
+          </div>
+          <div className="left test2">
+            <img></img>
+            <p>dummy text</p>
+          </div>
         </section>
         <section id="right-home">
-          <div className="right-test1">dummy text</div>
-          <div className="right-test2">dummy text</div>
+          <div className="right test1">
+            <img></img>
+            <p>dummy text</p>
+          </div>
+          <div className="right test2">
+            <img></img>
+            <p>dummy text</p>
+          </div>
         </section>
       </main>
     </>
